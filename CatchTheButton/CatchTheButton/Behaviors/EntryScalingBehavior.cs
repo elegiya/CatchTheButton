212e388639499ceb169be965057c184d6b05f592
elegiya <marina.a.gaponova@gmail.com>
2015-10-08 14:43:33 +0300

    All created at lesson project (previous used code is commented)

---
+++ CatchTheButton/CatchTheButton/Behaviors/EntryScalingBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CatchTheButton.Behaviors
+{
+    public class EntryScalingBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.Focused += Bindable_Focused;
+            bindable.Unfocused += Bindable_Unfocused;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.Focused -= Bindable_Focused;
+            bindable.Unfocused -= Bindable_Unfocused;
+            base.OnDetachingFrom(bindable);
+        }
+
+        async void Bindable_Unfocused(object sender, FocusEventArgs e)
+        {
+            var entry = (Entry)sender;
+            await entry.ScaleTo(1);
+        }
+
+        async void Bindable_Focused(object sender, FocusEventArgs e)
+        {
+            var entry = (Entry)sender;
+            await entry.ScaleTo(1.1);
+        }
+    }
+}
---
+++ CatchTheButton/CatchTheButton/Views/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using CatchTheButton.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CatchTheButton.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //tapMeButton.Clicked += OnTapMeButtonClicked;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            //var animation = new Animation(v => myLabel.Text = new string('.', (int)v), 1, 10);
+            //animation.Commit(this, "TextAnimation", 16, 1000);
+        }
+        //async void OnTapMeButtonClicked(object sender, EventArgs e)
+        //{
+        //    var button = (Button)sender;
+        //    var rnd = new Random();
+
+        //    var x = rnd.Next((int)tapMeLayot.Width);
+        //    var y = rnd.Next((int)tapMeLayot.Height);
+
+        //    var translateTo = button.TranslateTo(x, y, 500);
+
+        //    await button.ScaleTo(1.5, 250);
+        //    await button.ScaleTo(1, 250);
+
+        //    await button.RelRotateTo(360, 1000, Easing.SinIn);
+
+        //    await translateTo;
+
+        //    //AbsoluteLayout.SetLayoutBounds(button, new Rectangle(x, y, 150, 50));
+        //}
+    }
+}
